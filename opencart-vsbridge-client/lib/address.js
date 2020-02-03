c90e2036ffce9a202339dded39a6ad5d5c6f808c
Mohammad Tomaraei <mohammed.tomaraei@butopea.com>
2020-02-03 19:29:03 +0100

    added eslint exception

--- opencart-vsbridge-client/lib/address.js
+++ opencart-vsbridge-client/lib/address.js
@@ -1,34 +1,36 @@
 module.exports = function (restClient) {
   let module = {};
   let url = 'address/';
-  function getResponse(data){
-    if (data.code === 200){
+  function getResponse (data) {
+    if (data.code === 200) {
       return data.result;
     }
 
     return false;
   }
+  // eslint-disable-next-line no-unused-expressions
   module.list = function (customerToken) {
     url += `list?token=${customerToken}`
-    return restClient.get(url).then((data)=> {
+    return restClient.get(url).then((data) => {
       return getResponse(data);
     });
+    // eslint-disable-next-line no-sequences
   },
-  module.update = function (customerToken, addressData) {
-    url += `update?token=${customerToken}`
-    return restClient.post(url, {address: addressData}).then((data)=> {
-      return getResponse(data);
-    });
-  }
+      module.update = function (customerToken, addressData) {
+        url += `update?token=${customerToken}`
+        return restClient.post(url, {address: addressData}).then((data) => {
+          return getResponse(data);
+        });
+      }
   module.get = function (customerToken, addressId) {
     url += `get?token=${customerToken}&addressId=${addressId}`
-    return restClient.get(url).then((data)=> {
+    return restClient.get(url).then((data) => {
       return getResponse(data);
     });
   }
   module.delete = function (customerToken, addressData) {
     url += `delete?token=${customerToken}`
-    return restClient.post(url, {address: addressData}).then((data)=> {
+    return restClient.post(url, {address: addressData}).then((data) => {
       return getResponse(data);
     });
   }
